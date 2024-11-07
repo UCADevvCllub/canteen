fbecefb1190f988859f2916b2b91f6cdab9cc6be
Sunatullo <sunatullo.yogbekov_2027@ucentralasia.org>
2024-11-08 01:49:39 +0600

    login and signup pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
-import 'package:canteen/presentation/app.dart';
+// lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'presentation/pages/login_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(const App());
+void main() {
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const LoginPage(), // Starts with the login page
+    );
+  }
+}
