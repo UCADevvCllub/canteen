f74493803a2ba445bf64509b7d0fff2744e16be6
Sunatullo <sunatullo.yogbekov_2027@ucentralasia.org>
2024-12-01 20:53:50 +0600

    created widgets for catalog and product cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,8 @@
+import 'package:canteen/presentation/app.dart';
 import 'package:flutter/material.dart';
-import 'package:canteen/presentation/pages/catalog_page.dart'; // Import CatalogPage from the correct directory
-import 'package:canteen/presentation/pages/auth/login_page.dart';  // Import LoginPage from the correct directory
+import 'presentation/pages/auth/login_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Product Catalogue',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      initialRoute: '/catalog', // Start with CatalogPage for testing
-      routes: {
-        '/login': (context) => const LoginPage(),
-        '/catalog': (context) => const CatalogPage(),
-      },
-    );
-  }
-}
