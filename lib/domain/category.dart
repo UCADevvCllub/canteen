d0335c0023a010f76dc154e9dd3c359e48d97e9c
Student <student@users-MacBook-Pro.local>
2024-12-01 17:21:47 +0600

    models added

---
+++ lib/domain/category.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Category {
+  final String id;
+  final String name;
+  final String imageUrl;
+
+  Category({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  factory Category.fromFirebase(DocumentSnapshot snap) {
+    Map d = snap.data() as Map<String, dynamic>;
+    return Category(
+      id: d['id'],
+      name: d['name'],
+      imageUrl: d['imageUrl'],
+    );
+  }
+}
