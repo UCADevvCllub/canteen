9f687f8901bb403b09f0d78d2d28a424acb1ae57
Student <student@users-MacBook-Pro.local>
2024-12-05 01:27:00 +0600

    Global Theme Configured

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary color palette
+  static const Color primary = Color(0xFF84C264);
+  static const Color primaryVariant = Color(0xFF274119);
+  static const Color secondary = Color(0xFFF88F17);
+  static const Color secondaryVariant = Color(0xFFa45a05);
+
+  // Neutral colors
+  // static const Color background = Color(0xFFFFFFFF);
+  // static const Color surface = Color(0xFFFFFFFF);
+  // static const Color error = Color(0xFFB00020);
+  //
+  // // Text colors
+  // static const Color onPrimary = Colors.white;
+  // static const Color onSecondary = Colors.black;
+  static const Color onBackground = Colors.black;
+  // static const Color onSurface = Colors.black;
+  // static const Color onError = Colors.white;
+  //
+  // // Additional custom colors
+  // static const Color lightGray = Color(0xFFF5F5F5);
+  // static const Color darkGray = Color(0xFF9E9E9E);
+
+  static const Color darkGreen = Color(0xFF228C22);
+}
